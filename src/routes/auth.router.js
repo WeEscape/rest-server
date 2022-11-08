6bdd94d4b3817f88d8516ad1b4f0f3485b54420c
JoEunsoo <dcpg95@gmail.com>
2022-11-09 01:21:30 +0900

    CHORE : 초기세팅

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import { postLogin } from '../services/auth.service.js';
 
 const authRouter = express.Router();
 
-authRouter.route('/login').get().post();
+authRouter
+  .route('/')
+  .get((req, res) => res.send('helloworldzzzz!'))
+  .post((req, res, next) => {
+    const body = req.body;
+    console.log(body);
+    const login = postLogin(body);
+    res.send(login);
+  });
 
 export default authRouter;
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const userRouter = express.Router();
 
-userRouter.route('/user').get().post();
+// userRouter.route('/user').get('hellowuser').post();
 
 export default userRouter;
