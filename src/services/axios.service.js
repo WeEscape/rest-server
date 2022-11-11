775da28b6fed827f646175a5370d04ee2030f375
JoEunsoo <dcpg95@gmail.com>
2022-11-11 17:30:14 +0900

    FEAT : 외부 api 요청 로직

---
+++ src/services/axios.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const Oauth = async (data) => {
+  const { social_type, access_token } = data;
+  const loginTypeMap = {
+    kakao: kakaoLogin,
+    google: 'google',
+    apple: 'apple',
+  };
+  const login = await loginTypeMap[social_type](access_token);
+  //console.log(login);
+  return login;
+};
+
+const kakaoLogin = async (token) => {
+  try {
+    const response = await axios.get(process.env.KAKAO_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { id } = response.data;
+    const { nickname, profile_image } = response.data.properties;
+    return { id, type: 'kakao', nickname, profile_image };
+  } catch (err) {
+    return err;
+  }
+};
