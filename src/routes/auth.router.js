22379cd86ccd40f7bb631de532c30a7c6ed166b1
JoEunsoo <dcpg95@gmail.com>
2022-11-11 19:25:04 +0900

    REFACTOR : http 유형별 로직 분리

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -3,13 +3,12 @@ import { postLogin } from '../services/auth.service.js';
 
 const authRouter = express.Router();
 
-authRouter
-  .route('/')
-  .get((req, res) => res.send('helloworldzzzz!'))
-  .post(async (req, res, next) => {
-    const body = req.body;
-    const login = await postLogin(body);
-    res.send(login);
-  });
+authRouter.get('/', (req, res) => res.send('helloworldzzzz!'));
+
+authRouter.post('/', async (req, res, next) => {
+  const loginUserData = req.body;
+  const res_login = await postLogin(loginUserData);
+  res.send(res_login);
+});
 
 export default authRouter;
