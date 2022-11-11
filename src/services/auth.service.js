9e31f34f560b97e05a01ac6a2dc8a0116195e96a
JoEunsoo <dcpg95@gmail.com>
2022-11-11 19:25:36 +0900

    REFACTOR : 변수명변경, 각 요청별 로직 분리

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
-import { createJWT } from '../config/jwt.config.js';
 import { userTable } from '../models/user.model.js';
+import { createAccessToken, createRefreshToken } from '../utils/jwt.util.js';
 import { Oauth } from './axios.service.js';
 
-export const postLogin = async (data) => {
-  const { id } = await Oauth(data);
-  const runSQL = await userTable('select', id);
+export const postLogin = async (loginUserData) => {
+  const { socialId } = await Oauth(loginUserData);
+  const runSQL = await userTable('select', socialId);
   const { user_id } = runSQL[0];
-  const Token = await createJWT(user_id);
-  return Token;
+  const access_token = await createAccessToken(user_id);
+  const refresh_token = await createRefreshToken();
+  return { access_token, refresh_token };
 };
 
-const postSignup = async (data) => {
-  const userInfo = await Oauth(data);
+const postSignup = async (loginUserData) => {
+  const userInfo = await Oauth(loginUserData);
   const runSQL = (await userTable('insert', userInfo)) ? true : false;
   return runSQL;
 };
