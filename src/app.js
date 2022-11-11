4b7fdbd05da377122c430b475273e8af255af03a
JoEunsoo <dcpg95@gmail.com>
2022-11-11 17:26:48 +0900

    CHORE : 미들웨어 세팅

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
-import express, { Router } from 'express';
+import express from 'express';
 import cors from 'cors';
 import router from './routes/index.router.js';
 import userRouter from './routes/user.router.js';
 import authRouter from './routes/auth.router.js';
 import appConfig from './config/app.config.js';
+import helmet from 'helmet';
+
 const app = express();
 const { port } = appConfig;
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 
 app.use('/', router);
 app.use('/user', userRouter);
 app.use('/auth/login', authRouter);
 
 app.use((err, req, res, next) => res.json({ message: err.message }));
+
 app.listen(port, () =>
   console.log(`Open Server Port : ${process.env.PORT || port}`),
 );
