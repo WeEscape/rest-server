902b55b4e275dc7aa9088664910ab4dfd59eee56
JoEunsoo <dcpg95@gmail.com>
2022-11-12 22:34:14 +0900

    CHORE : 리프레시토큰 생성 함수에 인자 추가

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,18 +1,18 @@
-import { userTable } from '../models/user.model.js';
+import { UsersTable } from '../models/user.model.js';
 import { createAccessToken, createRefreshToken } from '../utils/jwt.util.js';
 import { Oauth } from './axios.service.js';
 
 export const postLogin = async (loginUserData) => {
   const { socialId } = await Oauth(loginUserData);
-  const runSQL = await userTable('select', socialId);
+  const runSQL = await UsersTable('select', socialId);
   const { user_id } = runSQL[0];
   const access_token = await createAccessToken(user_id);
-  const refresh_token = await createRefreshToken();
+  const refresh_token = await createRefreshToken(user_id);
   return { access_token, refresh_token };
 };
 
 const postSignup = async (loginUserData) => {
   const userInfo = await Oauth(loginUserData);
-  const runSQL = (await userTable('insert', userInfo)) ? true : false;
+  const runSQL = (await UsersTable('insert', userInfo)) ? true : false;
   return runSQL;
 };
