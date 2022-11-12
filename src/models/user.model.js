8b9e9e1861d1e7fa0e140a242894ff0b4f3f858d
JoEunsoo <dcpg95@gmail.com>
2022-11-12 22:32:58 +0900

    REFACTOR : 변수명 변경 , 로직 분리

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,17 +1,32 @@
 import { v4 } from 'uuid';
 import { dobbyDB } from './database.js';
 
-export const userTable = async (type, userinfo) => {
-  const sqlStatement = {
-    select: selectUser,
-    insert: insertUser,
-    update: updateUser,
-    delete: deleteUser,
-  };
+const selectUser = (socialId) => {
+  const userSelect = `SELECT * FROM dobby.USERS WHERE social_id='${socialId}';`;
+  return userSelect;
+};
 
+const insertUser = async (user_data) => {
+  const { id, type, nickname, profile_image } = user_data;
+  const userInsert = `INSERT INTO USERS(user_id,social_id,social_type,user_name,profile_url) VALUES('${v4()}','${id}','${type}','${nickname}','${profile_image}');`;
+  return userInsert;
+};
+
+const updateUser = () => {};
+
+const deleteUser = () => {};
+
+const sqlCommands = {
+  select: selectUser,
+  insert: insertUser,
+  update: updateUser,
+  delete: deleteUser,
+};
+
+export const UsersTable = async (sqlSyntax, userinfo) => {
   const connection = await dobbyDB.getConnection(async (conn) => conn);
   try {
-    const sql = await sqlStatement[type](userinfo);
+    const sql = await sqlCommands[sqlSyntax](userinfo);
     const resultSets = await connection.query(sql);
     // console.log(res[0]);
     return resultSets[0];
@@ -21,18 +36,3 @@ export const userTable = async (type, userinfo) => {
     connection.release();
   }
 };
-
-const selectUser = (socialId) => {
-  const userSelect = `SELECT * FROM dobby.USERS WHERE social_id='${socialId}';`;
-  return userSelect;
-};
-
-const insertUser = async (data) => {
-  const { id, type, nickname, profile_image } = data;
-  const userInsert = `INSERT INTO USERS(user_id,social_id,social_type,user_name,profile_url) VALUES('${v4()}','${id}','${type}','${nickname}','${profile_image}');`;
-  return userInsert;
-};
-
-const updateUser = () => {};
-
-const deleteUser = () => {};
