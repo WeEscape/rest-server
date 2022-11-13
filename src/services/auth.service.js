06337b76605c8ca25c73884de7108eb1183db4cb
JoEunsoo <dcpg95@gmail.com>
2022-11-13 22:10:52 +0900

    REFACTOR : 로그인 요청 조회 불가시 에러 추가

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,6 +6,7 @@ export const postLogin = async (loginUserData) => {
   const { socialId } = await Oauth(loginUserData);
   const runSQL = await UsersTable('select', socialId);
   const { user_id } = runSQL[0];
+  if (!user_id) throw err;
   const access_token = await createAccessToken(user_id);
   const refresh_token = await createRefreshToken(user_id);
   return { access_token, refresh_token };
