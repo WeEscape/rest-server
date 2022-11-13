68fa5eec56b630ab77bb25f43b20c2c27a93ad75
JoEunsoo <dcpg95@gmail.com>
2022-11-13 22:05:50 +0900

    FEAT : 리프레시 토큰 종료 날짜 로직 구현

--- src/utils/getDate.util.js
+++ src/utils/getDate.util.js
@@ -1,13 +1,14 @@
-export const getDate = async (createDate) => {
-  const year = createDate.getFullyear();
+export const getDate = async () => {
+  const createDate = new Date();
+  const year = createDate.getFullYear();
   const month = createDate.getMonth();
   const date = createDate.getDate();
   const hours = createDate.getHours();
   const minutes = createDate.getMinutes();
   const seconds = createDate.getSeconds();
 
-  const expiredDate = `${year}-${
-    month + 15
-  }-${date} ${hours}:${minutes}:${seconds}`;
+  const expiredDate = `${year}-${month}-${
+    date + 14
+  } ${hours}:${minutes}:${seconds}`;
   return expiredDate;
 };
