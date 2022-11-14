2922d6d14fb02a9c243d7b8d704968b7b6627f0f
JoEunsoo <dcpg95@gmail.com>
2022-11-14 21:33:43 +0900

    FEAT : 회원 조회 수정 로직

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,22 @@
-export const getUser = async (data) => {
-  return getUser;
+import { decodeAccessToken } from './jwt.service.js';
+import { UsersTable } from '../models/users.model.js';
+
+export const getUser = async (access_token) => {
+  try {
+    const decodedInfo = await decodeAccessToken(access_token);
+    const { id: user_id } = decodedInfo;
+    const userProfile = await UsersTable('select_userId', user_id);
+    return userProfile;
+  } catch (err) {
+    return err;
+  }
 };
 
-
+export const editUserProfile = async (update_date) => {
+  try {
+    const updateInfo = await UsersTable('update', update_date);
+    return updateInfo;
+  } catch (err) {
+    return err;
+  }
+};
