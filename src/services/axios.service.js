62fa2db772ceed37ce61e5d9e7c8b31df9957e87
JoEunsoo <dcpg95@gmail.com>
2022-11-14 21:31:03 +0900

    FEAT : 구글oauth api 요청 로직

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -4,7 +4,7 @@ export const Oauth = async (data) => {
   const { social_type, access_token } = data;
   const loginTypeMap = {
     kakao: kakaoLogin,
-    google: 'google',
+    google: googleLogin,
     apple: 'apple',
   };
   const login = await loginTypeMap[social_type](access_token);
@@ -20,8 +20,27 @@ const kakaoLogin = async (token) => {
       },
     });
     const { id: socialId } = response.data;
-    const { nickname, profile_image } = response.data.properties;
-    return { socialId, type: 'kakao', nickname, profile_image };
+    const { nickname: user_name, profile_image: profile_url } =
+      response.data.properties;
+    return { socialId, type: 'kakao', user_name, profile_url };
+  } catch (err) {
+    return err;
+  }
+};
+
+const googleLogin = async (token) => {
+  try {
+    const response = await axios.get(process.env.GOOGLE_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const {
+      id: socialId,
+      name: user_name,
+      picture: profile_url,
+    } = response.data;
+    return { socialId, type: 'google', user_name, profile_url };
   } catch (err) {
     return err;
   }
