42825b47a37f519a644eaf50e1e8fd631d7e8efd
JoEunsoo <dcpg95@gmail.com>
2022-11-14 21:30:08 +0900

    FEAT : 회원정보 수정 라우터

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import { editUserProfile, getUser } from '../services/user.service.js';
 
 const userRouter = express.Router();
 
-// userRouter.route('/user').get('hellowuser').post();
+userRouter.get('/', async (req, res) => {
+  const request_header = req.headers['authorization'];
+  if (!request_header)
+    return res.status(400).send({ message: 'access_token is not defined' });
+  const access_token = request_header.split('Bearer ')[1];
+  const userInfo = await getUser(access_token);
+  res.send(userInfo);
+});
+
+userRouter.put('/', async (req, res) => {
+    const update_date = req.body();
+    const userInfo = await editUserProfile(update_date);
+    res.send(userInfo)
+});
 
 export default userRouter;
