a1490cd4b20b3450a7d852227170d701dc8e670e
JoEunsoo <dcpg95@gmail.com>
2022-11-14 21:29:44 +0900

    REFACTOR : 회원가입 로그인 경로 분리

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,14 +1,24 @@
 import express from 'express';
-import { postLogin } from '../services/auth.service.js';
+import { postLogin, postSignup } from '../services/auth.service.js';
 
 const authRouter = express.Router();
 
 authRouter.get('/', (req, res) => res.send('helloworldzzzz!'));
 
-authRouter.post('/', async (req, res, next) => {
+authRouter.post('/login', async (req, res, next) => {
   const loginUserData = req.body;
   const res_login = await postLogin(loginUserData);
   res.send(res_login);
+});
+
+authRouter.post('/signup', async (req, res, next) => {
+  const socialTokens = req.body;
+  const res_sigunup = await postSignup(socialTokens);
+  try {
+    res.status(200).send({ message: 'success', status: 200 });
+  } catch {
+    res.status(400).send({ message: 'falid', status: 400 });
+  }
 });
 
 export default authRouter;
