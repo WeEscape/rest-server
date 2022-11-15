0626b28c4368962ffc6b2713de09110505f4631f
JoEunsoo <dcpg95@gmail.com>
2022-11-15 11:07:20 +0900

    FEAT : 로그아웃 로직 구현

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,13 +1,15 @@
 import { createRandomId } from '../utils/uniqueId.util.js';
 import { dobbyDB } from './database.js';
 
-const checkSignup = (socialId) => {
-  const socialId_Select = `SELECT * FROM dobby.USERS WHERE social_id='${socialId}';`;
-  return socialId_Select;
+const loginUser = (socialId) => {
+  const checkSocialid = `SELECT user_id FROM dobby.USERS WHERE social_id='${socialId}';`;
+  const loginUpdate = ` UPDATE dobby.USERS SET is_connect='1' WHERE social_id='${socialId}'`;
+  return checkSocialid + loginUpdate
 };
 
 const getUserInfo = (userId) => {
   const userId_Select = `SELECT * FROM dobby.USERS WHERE user_id='${userId}';`;
+  userId_Select;
   return userId_Select;
 };
 
@@ -20,19 +22,27 @@ const socialSingup = (user_data) => {
 
 const updateProfile = (user_data) => {
   const { user_id, user_name, profile_url, profile_color } = user_data;
-  const user_Update = `UPDATE USERS SET\
+  const profileUpdate = `UPDATE USERS SET\
   user_name=${user_name} profile_url=${profile_url} profile_color=${profile_color}\
    WHERE user_id=${user_id}`;
-  return user_Update;
+  return profileUpdate;
+};
+
+const logoutUser = (logoutData) => {
+  const { connect_Date } = logoutData;
+  const logoutUpdate = `UPDATE USERS SET\
+  is_connect=${0} last_connected_at=${connect_Date} WHERE user_id=${user_id}
+  `;
 };
 
 const deleteUser = () => {};
 
 const sqlCommands = {
-  select_socialId: checkSignup,
+  login: loginUser,
   select_userId: getUserInfo,
   insert_newUser: socialSingup,
-  update: updateProfile,
+  editProfile: updateProfile,
+  logout: logoutUser,
   delete: deleteUser,
 };
 
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,12 @@
 import { UsersTable } from '../models/users.model.js';
+import { getDate } from '../utils/getDate.util.js';
 import { createAccessToken, createRefreshToken } from './/jwt.service.js';
 import { Oauth } from './axios.service.js';
 
 export const postLogin = async (loginUserData) => {
   const { socialId } = await Oauth(loginUserData);
-  const runSQL = await UsersTable('select_socialId', socialId);
-  const { user_id } = runSQL[0];
-  if (!user_id) throw err;
+  const runSQL = await UsersTable('login', socialId);
+  const { user_id } = runSQL[0][0];
   const access_token = await createAccessToken(user_id);
   const refresh_token = await createRefreshToken(user_id);
   return { access_token, refresh_token };
@@ -14,7 +14,14 @@ export const postLogin = async (loginUserData) => {
 
 export const postSignup = async (socialTokens) => {
   const userInfo = await Oauth(socialTokens);
-  console.log(userInfo);
   const runSQL = await UsersTable('insert_newUser', userInfo);
+  return runSQL;
+};
+
+export const postLogout = async (logoutUser_id) => {
+  const { user_id } = logoutUser_id;
+  const connect_Date = getDate();
+  const logoutData = { user_id, connect_Date };
+  const runSQL = await UsersTable('editProfile', logoutData);
   return runSQL;
 };
