4af32dd9b08bf47db6ec98d7483fbd3a117bdc45
JoEunsoo <dcpg95@gmail.com>
2022-11-16 23:05:23 +0900

    REFACTOR : jwtdecode 미들웨어 구현 authorization header 코드수정

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,28 +1,47 @@
 import express from 'express';
-import { editUserProfile, getUser } from '../services/user.service.js';
+import { decodeAccessToken } from '../services/jwt.service.js';
+import {
+  deleteUser,
+  editUserProfile,
+  getUser,
+} from '../services/user.service.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/', async (req, res) => {
-  const request_header = req.headers['authorization'];
-  if (!request_header)
+userRouter.get('/', async (req, res, next) => {
+  try {
+    const request_header = req.headers['authorization'];
+    const access_token = request_header.split(' ')[1];
+    const { id: user_id } = await decodeAccessToken(access_token);
+    if (!user_id) throw new Error('undefined');
+    const resultUserInfo = await getUser(user_id);
+    return res.send({ data: resultUserInfo });
+  } catch (err) {
     return res.status(400).send({ message: 'access_token is not defined' });
-  const access_token = request_header.split('Bearer ')[1];
-  const userInfo = await getUser(access_token);
-  res.send(userInfo);
+  }
 });
 
-userRouter.put('/', async (req, res) => {
+userRouter.put('/', async (req, res, next) => {
+  try {
     const update_date = req.body();
-    const userInfo = await editUserProfile(update_date);
-    res.send(userInfo)
+    const resultUserInfo = await editUserProfile(update_date);
+    return res.send({ data: resultUserInfo });
+  } catch (err) {
+    next(err);
+  }
 });
 
-userRouter.delete('/', async (req,res) => {
-  const request_header = req.headers['authorization'];
-  if (!request_header)
-  return res.status(400).send({ message: 'access_token is not defined' });
-  const access_token = request_header.split('Bearer ')[1];
-})
+userRouter.delete('/', async (req, res, next) => {
+  try {
+    const request_header = req.headers['authorization'];
+    const access_token = request_header.split(' ')[1];
+    const { id: user_id } = await decodeAccessToken(access_token);
+    if (!user_id) throw new Error('undefined');
+    const resultDelete = await deleteUser(user_id);
+    return res.send({ message: 'success delete' });
+  } catch (err) {
+    return res.status(400).send({ message: 'access_token is not defined' });
+  }
+});
 
 export default userRouter;
