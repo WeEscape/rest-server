35716ee7806eb8f0948c595209b5b519ee66dadc
JoEunsoo <dcpg95@gmail.com>
2022-11-16 23:03:32 +0900

    REFACTOR : 함수명 변경

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,14 +5,14 @@ import { createAccessToken, createRefreshToken } from './/jwt.service.js';
 import { Oauth } from './axios.service.js';
 
 // 회원가입
-export const postSignup = async (socialTokens) => {
+export const signup = async (socialTokens) => {
   const userInfo = await Oauth(socialTokens);
   const runSQL = await UsersTable('insert_newUser', userInfo);
   return runSQL;
 };
 
 // 로그인
-export const postLogin = async (loginUserData) => {
+export const login = async (loginUserData) => {
   const { socialId } = await Oauth(loginUserData);
   const runSQL = await UsersTable('login', socialId);
   const { user_id } = runSQL[0][0];
@@ -23,9 +23,9 @@ export const postLogin = async (loginUserData) => {
 };
 
 // 로그아웃
-export const postLogout = async (logoutUser_id) => {
+export const logout = async (logoutUser_id) => {
   const { user_id } = logoutUser_id;
-  if(!user_id) throw new Error('user_id not defined');
+  if (!user_id) throw new Error('user_id not defined');
   const connect_Date = getDate();
   const logoutData = { user_id, connect_Date };
   const runSQL = await UsersTable('editProfile', logoutData);
@@ -34,7 +34,8 @@ export const postLogout = async (logoutUser_id) => {
 
 // 토큰 재발급
 export const reissueTokens = async (refreshToken) => {
-  const runSQL = RefreshTokenTable('checkToken', refreshToken);
+  const runSQL = await RefreshTokenTable('checkToken', refreshToken);
+  console.log(runSQL);
   if (!runSQL) throw { message: 'refresh_token Not defiend!' };
   const { user_id } = runSQL[0];
   const access_token = await createAccessToken(user_id);
