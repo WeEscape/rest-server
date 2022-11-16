761893110e816eba9e6da152569380b1bf898471
JoEunsoo <dcpg95@gmail.com>
2022-11-16 23:03:07 +0900

    REFACTOR : 불필요한 try catch 제거

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,33 +1,16 @@
-import { decodeAccessToken } from './jwt.service.js';
 import { UsersTable } from '../models/users.model.js';
 
-export const getUser = async (access_token) => {
-  try {
-    const decodedInfo = await decodeAccessToken(access_token);
-    const { id: user_id } = decodedInfo;
-    const userProfile = await UsersTable('select_userId', user_id);
-    return userProfile;
-  } catch (err) {
-    return err;
-  }
+export const getUser = async (user_id) => {
+  const userProfile = await UsersTable('select_userId', user_id);
+  return userProfile[0];
 };
 
 export const editUserProfile = async (update_date) => {
-  try {
-    const updateInfo = await UsersTable('update', update_date);
-    return updateInfo;
-  } catch (err) {
-    return err;
-  }
+  const updateInfo = await UsersTable('update', update_date);
+  return updateInfo;
 };
 
-export const deleteUser = async (access_token) => {
-  try {
-    const decodedInfo = await decodeAccessToken(access_token);
-    const { id: user_id } = decodedInfo;
-    const successDelete = await UsersTable('delete', user_id);
-    return successDelete;
-  } catch (err) {
-    return err;
-  }
+export const deleteUser = async (user_id) => {
+  const successDelete = await UsersTable('delete', user_id);
+  return successDelete;
 };
