4b257b6f475d32cf0214a84dc815f1bce074e390
JoEunsoo <dcpg95@gmail.com>
2022-11-16 23:07:12 +0900

    CHORE : import 변수 변경

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { postLogin, postLogout, postSignup } from '../services/auth.service.js';
+import {
+  login,
+  logout,
+  signup,
+  reissueTokens,
+} from '../services/auth.service.js';
 
 const authRouter = express.Router();
 
@@ -8,19 +13,20 @@ authRouter.get('/', (req, res) => res.send('helloworldzzzz!'));
 authRouter.post('/login', async (req, res, next) => {
   try {
     const loginUserData = req.body;
-    const res_login = await postLogin(loginUserData);
-    res.status(200).send(res_login);
-  } catch(err) {
+    console.log(loginUserData);
+    const resultLogin = await login(loginUserData);
+    return res.status(200).send({ data: resultLogin });
+  } catch (err) {
     next(err);
   }
 });
 
 authRouter.post('/signup', async (req, res, next) => {
   const socialTokens = req.body;
-  const res_sigunup = await postSignup(socialTokens);
+  const resultsigunup = await signup(socialTokens);
   try {
-    res.status(200).send({ message: 'success login', status: 200 });
-  } catch(err) {
+    return res.status(200).send({ data: resultsigunup });
+  } catch (err) {
     next(err);
   }
 });
@@ -28,15 +34,21 @@ authRouter.post('/signup', async (req, res, next) => {
 authRouter.post('/logout', async (req, res, next) => {
   try {
     const logoutUser_id = req.body;
-    const res_logout = await postLogout(logoutUser_id);
-    res.status(200).send({message: 'success logout', status: 200 });
-  } catch(err) {
+    const resultLogout = await logout(logoutUser_id);
+    return res.status(200).send({ data: resultLogout });
+  } catch (err) {
     next(err);
   }
 });
 
 authRouter.post('/tokens', async (req, res, next) => {
-  const refreshToken = req.body;
+  try {
+    const refreshToken = req.body;
+    const resultTokens = await reissueTokens(refreshToken);
+    return res.status(200).send({ data: resultTokens });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default authRouter;
