16736480027fd7b9dd2747bb532b4419e71e4a3a
JoEunsoo <dcpg95@gmail.com>
2022-11-19 01:18:47 +0900

    FEAT : 카테고리 CRUD 비즈니스 로직 구현

---
+++ src/services/categories.service.js
@@ -0,0 +1,25 @@
+const createCategory = async (categoriesTable, categoryData) => {
+  const runSQL = await categoriesTable('insert', categoryData);
+  return runSQL[1][0];
+};
+const getCategory = async (categoriesTable, categoryData) => {
+  const runSQL = await categoriesTable('select', categoryData);
+  console.log(runSQL);
+  return { data: runSQL[0] };
+};
+const editCategory = async (categoriesTable, categoryData) => {
+  const runSQL = await categoriesTable('update', categoryData);
+  console.log(runSQL);
+  return runSQL[1][0];
+};
+const deleteCategory = async (categoriesTable, categoryData) => {
+  const runSQL = await categoriesTable('delete', categoryData);
+  return runSQL;
+};
+
+export const CategoriesService = {
+  createCategory,
+  getCategory,
+  editCategory,
+  deleteCategory,
+};
