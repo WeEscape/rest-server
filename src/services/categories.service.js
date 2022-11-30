8b5e8d3a8d3aebfbe5aaf7d40d550245b9530813
JoEunsoo <dcpg95@gmail.com>
2022-12-01 08:34:04 +0900

    REFACTOR : 카테고리 서비스 코드 리팩토링

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -1,18 +1,28 @@
-const createCategory = async (categoriesTable, categoryData) => {
-  const runSQL = await categoriesTable('insert', categoryData);
-  return runSQL[1][0];
+import { CategoriesModel } from '../models/categories.model.js';
+import { TableQuery } from '../models/database.js';
+
+const createCategory = async (categoryData) => {
+  const sql = await CategoriesModel.insertCategory(categoryData);
+  const category = await TableQuery(sql);
+  return category[1][0];
 };
-const getCategory = async (categoriesTable, categoryData) => {
-  const runSQL = await categoriesTable('select', categoryData);
-  return { data: runSQL[0] };
+
+const getCategory = async (categoryData) => {
+  const sql = await CategoriesModel.selectCategory(categoryData);
+  const category = await TableQuery(sql);
+  return { data: category[0] };
 };
-const editCategory = async (categoriesTable, categoryData) => {
-  const runSQL = await categoriesTable('update', categoryData);
-  return runSQL[1][0];
+
+const editCategory = async (categoryData) => {
+  const sql = await CategoriesModel.updateCategory(categoryData);
+  const category = await TableQuery(sql);
+  return category[1][0];
 };
-const deleteCategory = async (categoriesTable, categoryData) => {
-  const runSQL = await categoriesTable('delete', categoryData);
-  return runSQL;
+
+const deleteCategory = async (categoryData) => {
+  const sql = await CategoriesModel.deleteCategory(categoryData);
+  const category = await TableQuery(sql);
+  return category;
 };
 
 export const CategoriesService = {
