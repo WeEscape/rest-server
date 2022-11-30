b7056931808706491d0b720967b47635764ba0f3
JoEunsoo <dcpg95@gmail.com>
2022-12-01 08:37:39 +0900

    REFACTOR : 테스크 서비스불필요한 매개변수 제거

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,22 +1,22 @@
 import { TableQuery } from '../models/database.js';
 import { TaskModel } from '../models/tasks.model.js';
 
-const createTask = async (taskTable, taskData) => {
+const createTask = async (taskData) => {
   const sql = await TaskModel.insertTask(taskData);
   const task = await TableQuery(sql);
   return task[1][0];
 };
-const getTask = async (taskTable, taskData) => {
+const getTask = async (taskData) => {
   const sql = await TaskModel.selectTask(taskData);
   const task = await TableQuery(sql);
   return { data: task[0] };
 };
-const editTask = async (taskTable, taskData) => {
+const editTask = async (taskData) => {
   const sql = await TaskModel.updateTask(taskData);
   const task = await TableQuery(sql);
   return task[1][0];
 };
-const deleteTask = async (taskTable, taskData) => {
+const deleteTask = async (taskData) => {
   const sql = await TaskModel.deleteTask(taskData);
   const task = await TableQuery(sql);
   return task;
