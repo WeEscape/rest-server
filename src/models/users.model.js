65418f5ae23911906be8ad4259a886d0f07f7e5b
JoEunsoo <dcpg95@gmail.com>
2022-11-30 23:36:11 +0900

    REFACTOR : 쿼리실행 로직 분리

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,26 +1,26 @@
 import { createRandomId } from '../utils/randomString.util.js';
-import { dobbyDB } from './database.js';
 
-const selectUserid = (socialId) => {
+const selectUserid = async (socialId) => {
   const checkSocialid = `SELECT user_id FROM USERS WHERE social_id='${socialId}';`;
   const loginUpdate = ` UPDATE USERS SET is_connect='1' WHERE social_id='${socialId}'`;
   return checkSocialid + loginUpdate;
 };
 
-const selectUserInfo = (user_id) => {
+const selectUserInfo = async (user_id) => {
   const userId_Select = `SELECT * FROM USERS WHERE user_id='${user_id}'`;
   userId_Select;
   return userId_Select;
 };
 
-const insertUser = (user_data) => {
+// 회원가입
+const insertUser = async (user_data) => {
   const { socialId, type, user_name, profile_url } = user_data;
   const userInsert = `INSERT INTO USERS(user_id,social_id,social_type,user_name,profile_url)\ 
   VALUES('${createRandomId()}','${socialId}','${type}','${user_name}','${profile_url}');`;
   return userInsert;
 };
 
-const updateProfile = (user_data) => {
+const updateProfile = async (user_data) => {
   const { user_id, user_name, profile_url, profile_color } = user_data;
   const profileUpdate = `UPDATE USERS SET\
   user_name=${user_name} profile_url=${profile_url} profile_color=${profile_color}\
@@ -28,41 +28,24 @@ const updateProfile = (user_data) => {
   return profileUpdate;
 };
 
-const updateConnect = (logout_Data) => {
+const updateConnect = async (logout_Data) => {
   const { connect_Date } = logout_Data;
   const logoutUpdate = `UPDATE USERS SET\
   is_connect=0 last_connected_at=${connect_Date} WHERE user_id=${user_id}
   `;
 };
 
-const deleteProfile = (user_id) => {
+const deleteProfile = async (user_id) => {
   const profile_Delete = `DELETE FROM USERS WHERE user_id='${user_id}';`;
   const refreshToken_Delete = `DELETE FROM USERS_REFRESH_TOKENS WHERE user_id='${user_id}'`;
   return profile_Delete + refreshToken_Delete;
 };
 
-const sqlCommands = {
-  login: selectUserid,
-  select_userId: selectUserInfo,
-  insert_newUser: insertUser,
-  editProfile: updateProfile,
-  logout: updateConnect,
-  delete: deleteProfile,
-};
-
-// USERS 테이블
-export const UsersTable = async (sqlSyntax, userinfo) => {
-  const connection = await dobbyDB.getConnection(async (conn) => conn);
-  try {
-    const sql = await sqlCommands[sqlSyntax](userinfo);
-    await connection.beginTransaction();
-    const resultSets = await connection.query(sql);
-    await connection.commit();
-    return resultSets[0];
-  } catch (err) {
-    await connection.rollback();
-    return err;
-  } finally {
-    connection.release();
-  }
+export const UsersModel = {
+  selectUserid,
+  selectUserInfo,
+  insertUser,
+  updateProfile,
+  updateConnect,
+  deleteProfile,
 };
