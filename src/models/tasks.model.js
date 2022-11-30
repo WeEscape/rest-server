976b776612997c3d047d8a6bc0e0d240cc86f901
JoEunsoo <dcpg95@gmail.com>
2022-11-30 23:36:25 +0900

    REFACTOR : 쿼리실행 로직 분리

--- src/models/tasks.model.js
+++ src/models/tasks.model.js
@@ -1,6 +1,6 @@
 import { getDate } from '../utils/getDate.util.js';
 
-const insertTask = (taskData) => {
+const insertTask = async (taskData) => {
   const { user_id, category_id } = taskData;
   const insertSQL = `INSERT INTO TASKS (task_id, user_id, category_id, notice_available,title) VALUES\
   ('${createRandomId()}','${user_id}','${category_id}','1');
@@ -9,44 +9,28 @@ const insertTask = (taskData) => {
   return insertSQL + selectSQL;
 };
 
-const selectTask = (taskData) => {
+const selectTask = async (taskData) => {
   const { user_id, task_id } = taskData;
   const selectSQL = `SELECT * FROM TASKS WHERE user_id='${user_id}' AND task_id=${task_id}`;
   return selectSQL;
 };
 
-const updateTask = (taskData) => {
+const updateTask = async (taskData) => {
   const { excute_at, memo } = taskData;
   const updateSQL = `UPDATE TASKS SET excute_at=${excute_at} memo=${memo}
   FROM WHERE user_id='${user_id}' AND task_id=${task_id}`;
   return updateSQL;
 };
 
-const deleteTask = (taskData) => {
+const deleteTask = async (taskData) => {
   const { user_id, task_id } = taskData;
   const deleteSQL = `DELETE FROM TASKS WHERE task_id = '${task_id}' AND user_id = '${user_id}'`;
   return deleteSQL;
 };
 
-const sqlCommands = {
-  insert: insertTask,
-  select: selectTask,
-  update: updateTask,
-  delete: deleteTask,
-};
-
-export const taskTable = async (sqlSyntax, userinfo) => {
-  const connection = await dobbyDB.getConnection(async (conn) => conn);
-  try {
-    const sql = await sqlCommands[sqlSyntax](userinfo);
-    await connection.beginTransaction();
-    const resultSets = await connection.query(sql);
-    await connection.commit();
-    return resultSets[0];
-  } catch (err) {
-    await connection.rollback();
-    return err;
-  } finally {
-    connection.release();
-  }
+export const TaskModel = {
+  insertTask,
+  selectTask,
+  updateTask,
+  deleteTask,
 };
