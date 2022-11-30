677cf8437863a4a3f668295d1d87ec1986abacd9
JoEunsoo <dcpg95@gmail.com>
2022-12-01 08:16:33 +0900

    REFACTOR : 불필요한 코드 제거

--- src/models/groups.model.js
+++ src/models/groups.model.js
@@ -1,10 +1,9 @@
-import { dobbyDB } from './database.js';
 import {
   createInviteCode,
   createRandomId,
 } from '../utils/randomString.util.js';
 
-const insertGroup = (group_data) => {
+const insertGroup = async (group_data) => {
   const { user_id, title } = group_data;
   const insertSQL = `INSERT INTO GROUPS (group_id, user_id,title,invite_code) VALUES\
     ('${createRandomId()}','${user_id}','${title}','${createInviteCode()}');`;
@@ -12,19 +11,19 @@ const insertGroup = (group_data) => {
   return insertSQL + selectSQL;
 };
 
-const selectGroup = (group_data) => {
+const selectGroup = async (group_data) => {
   const { group_id, user_id } = group_data;
   const selectSQL = `SELECT * FROM GROUPS WHERE group_id='${group_id}' AND user_id='${user_id}'`;
   return selectSQL;
 };
 
-const updateGroup = (groupData) => {
+const updateGroup = async (groupData) => {
   const { title, group_id } = groupData;
   const updateSQL = `UPDATE GROUPS SET title='${title}' WHERE group_id='${group_id}'`;
   return updateSQL;
 };
 
-const deleteGroup = (groupData) => {
+const deleteGroup = async (groupData) => {
   const { group_id, user_id } = groupData;
   const deleteSQL = `DELETE t1, t2 FROM GROUPS t1 LEFT JOIN GROUPS_USERS t2 ON t1.user_id = t2.user_id\
   WHERE t1.group_id = '${group_id}' AND t1.user_id = '${user_id}' ;`;
@@ -32,26 +31,32 @@ const deleteGroup = (groupData) => {
   return deleteSQL;
 };
 
-const sqlCommands = {
-  insert: insertGroup,
-  select: selectGroup,
-  update: updateGroup,
-  delete: deleteGroup,
+export const GroupsModel = {
+  insertGroup,
+  selectGroup,
+  updateGroup,
+  deleteGroup,
 };
 
-// GROUPS 테이블
-export const GroupsTable = async (sqlSyntax, userinfo) => {
-  const connection = await dobbyDB.getConnection(async (conn) => conn);
-  try {
-    const sql = await sqlCommands[sqlSyntax](userinfo);
-    await connection.beginTransaction();
-    const resultSets = await connection.query(sql);
-    await connection.commit();
-    return resultSets[0];
-  } catch (err) {
-    await connection.rollback();
-    return err;
-  } finally {
-    connection.release();
-  }
+const insertGroupUser = async (groupUser_data) => {
+  const { group_id, user_id } = groupUser_data;
+  const insertSQL = `INSERT INTO GROUPS_USERS (group_id, user_id) VALUES ('${group_id}','${user_id}');`;
+  const selectSQL = `SELECT * FROM GROUPS_USERS WHERE  user_id='${user_id}'`;
+  return insertSQL + selectSQL;
+};
+const selectGroupUser = async (groupUser_data) => {
+  const { group_id, user_id } = groupUser_data;
+  const selectSQL = `SELECT * FROM GROUPS_USERS WHERE group_id='${group_id}' AND user_id='${user_id}'`;
+  return selectSQL;
+};
+const deleteGroupUser = async (groupUser_data) => {
+  const { group_id, user_id } = groupUser_data;
+  const deleteSQL = `DELETE FROM GROUPS_USERS WHERE group_id = '${group_id}' AND user_id = '${user_id}'`;
+  return deleteSQL;
+};
+
+export const GroupUsersModel = {
+  insertGroupUser,
+  selectGroupUser,
+  deleteGroupUser,
 };
