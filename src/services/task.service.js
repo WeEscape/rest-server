03ec3644c06c9c1d4ec3ab779f41b6161981472a
JoEunsoo <dcpg95@gmail.com>
2022-11-30 23:37:51 +0900

    REFACTOR : 불필요한 코드 제거

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,18 +1,25 @@
+import { TableQuery } from '../models/database';
+import { TaskModel } from '../models/tasks.model';
+
 const createTask = async (taskTable, taskData) => {
-  const runSQL = taskTable('insert', taskData);
-  return runSQL[1][0];
+  const sql = await TaskModel.insertTask(taskData);
+  const task = await TableQuery(sql);
+  return task[1][0];
 };
 const getTask = async (taskTable, taskData) => {
-  const runSQL = await taskTable('select', taskData);
-  return { data: runSQL[0] };
+  const sql = await TaskModel.selectTask(taskData);
+  const task = await TableQuery(sql);
+  return { data: task[0] };
 };
 const editTask = async (taskTable, taskData) => {
-  const runSQL = await taskTable('update', taskData);
-  return runSQL[1][0];
+  const sql = await TaskModel.updateTask(taskData);
+  const task = await TableQuery(sql);
+  return task[1][0];
 };
 const deleteTask = async (taskTable, taskData) => {
-  const runSQL = await taskTable('delete', taskData);
-  return runSQL;
+  const sql = await TaskModel.deleteTask(taskData);
+  const task = await TableQuery(sql);
+  return task;
 };
 
 const createTaskUser = async () => {};
