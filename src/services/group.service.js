5a5edb88c854427598f072188634d1aa137e9677
JoEunsoo <dcpg95@gmail.com>
2022-12-01 08:17:05 +0900

    REFACTOR : 그룹 서비스 로직 코드 리팩토링

--- src/services/group.service.js
+++ src/services/group.service.js
@@ -1,39 +1,49 @@
+import { TableQuery } from '../models/database.js';
+import { GroupsModel, GroupUsersModel } from '../models/groups.model.js';
+
 // 그룹
 const createGroup = async (groupTable, groupData) => {
-  const runSQL = await groupTable('insert', groupData);
-  return runSQL[1][0];
+  const sql = await GroupsModel.insertGroup(groupData);
+  const group = TableQuery(sql);
+  return group[1][0];
 };
 
 const getGroup = async (groupTable, groupData) => {
-  const runSQL = await groupTable('select', groupData);
-  return { data: runSQL[0] };
+  const sql = await GroupsModel.selectGroup(groupData);
+  const group = await TableQuery(sql);
+  return { data: group[0] };
 };
 
 const editGroup = async (groupTable, groupData) => {
-  const runSQL = await groupTable('update', groupData);
-  return { data: runSQL[0] };
+  const sql = await GroupsModel.updateGroup(groupData);
+  const group = await TableQuery(sql);
+  return { data: group[0] };
 };
 
 const deleteGroup = async (groupTable, groupData) => {
-  const runSQL = await groupTable('delete', groupData);
-  return { data: runSQL[0] };
+  const sql = await GroupsModel.deleteGroup(groupData);
+  const group = await TableQuery(sql);
+  return { data: group[0] };
 };
 
 // 그룹유저
 const createGroupUser = async (groupsUsersTable, groupUesr_data) => {
-  const runSQL = await groupsUsersTable('insert', groupUesr_data);
-  console.log(runSQL);
-  return runSQL[1][0];
+  const sql = await GroupUsersModel.insertGroupUser(groupUesr_data);
+  const groupUser = await TableQuery(sql);
+  console.log(groupUser);
+  return groupUser[1][0];
 };
 
 const getGroupUser = async (groupsUsersTable, groupUesr_data) => {
-  const runSQL = await groupsUsersTable('select', groupUesr_data);
-  return { data: runSQL[0] };
+  const sql = await GroupUsersModel.selectGroupUser(groupUesr_data);
+  const groupUser = await TableQuery(sql);
+  return { data: groupUser[0] };
 };
 
 const deleteGroupUser = async (groupsUsersTable, groupUesr_data) => {
-  const runSQL = await groupsUsersTable('delete', groupUesr_data);
-  return runSQL;
+  const sql = await GroupUsersModel.deleteGroupUser(groupUesr_data);
+  const groupUser = await TableQuery(sql);
+  return groupUser;
 };
 
 export const GroupService = {
