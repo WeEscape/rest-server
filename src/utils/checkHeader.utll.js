3b64e126400b27d20a427ed475975e2980545cc4
JoEunsoo <dcpg95@gmail.com>
2022-12-01 09:41:00 +0900

    FEAT : 토큰검사 로직 구현

--- src/utils/checkHeader.utll.js
+++ src/utils/checkHeader.utll.js
@@ -1,6 +1,8 @@
-export const checkRequestHeader = async (req) => {
-  const request_header = req.headers['authorization'];
-  const result = request_header.split(' ')[1];
+import { decodeAccessToken } from './decodeToken.util.js';
 
-  return result;
+export const checkAccessToken = async (req) => {
+  const request_header = req.headers['authorization'];
+  const access_token = request_header.split(' ')[1];
+  const { id: user_id } = await decodeAccessToken(access_token);
+  return user_id;
 };
