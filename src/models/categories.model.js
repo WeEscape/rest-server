99ab94710c1459105f4bcbc3e8c2a30f858523c4
JoEunsoo <dcpg95@gmail.com>
2022-12-07 23:52:01 +0900

    CHORE : 불필요한 코드 제거

--- src/models/categories.model.js
+++ src/models/categories.model.js
@@ -1,7 +1,7 @@
 import { createRandomId } from '../utils/randomString.util.js';
 import { dobbyDB } from './database.js';
 
-const insertCategory = async (categoryData) => {
+const insertCategory = (categoryData) => {
   const { user_id, group_id, title } = categoryData;
   const insertSQL = `INSERT INTO CATEGORIES (category_id, user_id,group_id,title) VALUES\
     ('${createRandomId()}','${user_id}','${group_id}','${title}');
@@ -10,20 +10,20 @@ const insertCategory = async (categoryData) => {
   return insertSQL + selectSQL;
 };
 
-const selectCategory = async (categoryData) => {
+const selectCategory = (categoryData) => {
   const { user_id, category_id } = categoryData;
   const selectSQL = `SELECT * FROM CATEGORIES WHERE user_id='${user_id}' AND category_id='${category_id}'`;
   return selectSQL;
 };
 
-const updateCategory = async (categoryData) => {
+const updateCategory = (categoryData) => {
   const { category_id, user_id, title } = categoryData;
   const updateSQL = `UPDATE CATEGORIES SET title='${title}' WHERE category_id='${category_id}' AND user_id='${user_id}';`;
   const selectSQL = `SELECT * FROM CATEGORIES WHERE category_id='${category_id}' AND user_id='${user_id}'`;
   return updateSQL + selectSQL;
 };
 
-const deleteCategory = async (categoryData) => {
+const deleteCategory = (categoryData) => {
   const { user_id, category_id } = categoryData;
   const deleteSQL = `DELETE FROM CATEGORIES WHERE category_id = '${category_id}' AND user_id = '${user_id}'`;
   console.log(deleteSQL);
--- src/models/groups.model.js
+++ src/models/groups.model.js
@@ -3,7 +3,7 @@ import {
   createRandomId,
 } from '../utils/randomString.util.js';
 
-const insertGroup = async (group_data) => {
+const insertGroup = (group_data) => {
   const { user_id, title } = group_data;
   const insertSQL = `INSERT INTO GROUPS (group_id, user_id,title,invite_code) VALUES\
     ('${createRandomId()}','${user_id}','${title}','${createInviteCode()}');`;
@@ -11,19 +11,19 @@ const insertGroup = async (group_data) => {
   return insertSQL + selectSQL;
 };
 
-const selectGroup = async (group_data) => {
+const selectGroup = (group_data) => {
   const { group_id, user_id } = group_data;
   const selectSQL = `SELECT * FROM GROUPS WHERE group_id='${group_id}' AND user_id='${user_id}'`;
   return selectSQL;
 };
 
-const updateGroup = async (groupData) => {
+const updateGroup = (groupData) => {
   const { title, group_id, uesr_id } = groupData;
   const updateSQL = `UPDATE GROUPS SET title='${title}' WHERE group_id='${group_id}' AND user_id='${uesr_id}'`;
   return updateSQL;
 };
 
-const deleteGroup = async (groupData) => {
+const deleteGroup = (groupData) => {
   const { group_id, user_id } = groupData;
   const deleteSQL = `DELETE t1, t2 FROM GROUPS t1 LEFT JOIN GROUPS_USERS t2 ON t1.user_id = t2.user_id\
   WHERE t1.group_id = '${group_id}' AND t1.user_id = '${user_id}' ;`;
@@ -38,18 +38,18 @@ export const GroupsModel = {
   deleteGroup,
 };
 
-const insertGroupUser = async (groupUser_data) => {
+const insertGroupUser = (groupUser_data) => {
   const { group_id, user_id } = groupUser_data;
   const insertSQL = `INSERT INTO GROUPS_USERS (group_id, user_id) VALUES ('${group_id}','${user_id}');`;
   const selectSQL = `SELECT * FROM GROUPS_USERS WHERE  user_id='${user_id}'`;
   return insertSQL + selectSQL;
 };
-const selectGroupUser = async (groupUser_data) => {
+const selectGroupUser = (groupUser_data) => {
   const { group_id, user_id } = groupUser_data;
   const selectSQL = `SELECT * FROM GROUPS_USERS WHERE group_id='${group_id}' AND user_id='${user_id}'`;
   return selectSQL;
 };
-const deleteGroupUser = async (groupUser_data) => {
+const deleteGroupUser = (groupUser_data) => {
   const { group_id, user_id } = groupUser_data;
   const deleteSQL = `DELETE FROM GROUPS_USERS WHERE group_id = '${group_id}' AND user_id = '${user_id}'`;
   return deleteSQL;
