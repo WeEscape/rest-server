67fcda62005240ea5a8536e8c98c39f8204547d3
JoEunsoo <dcpg95@gmail.com>
2022-12-07 23:51:55 +0900

    CHORE : 불필요한 코드 제거

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,25 +1,25 @@
 import { createRandomId } from '../utils/randomString.util.js';
 
-const selectUserid = async (socialId) => {
+const selectUserid = (socialId) => {
   const checkSocialid = `SELECT user_id FROM USERS WHERE social_id='${socialId}';`;
   const loginUpdate = ` UPDATE USERS SET is_connect='1' WHERE social_id='${socialId}'`;
   return checkSocialid + loginUpdate;
 };
 
-const selectUserInfo = async (user_id) => {
+const selectUserInfo = (user_id) => {
   const userId_Select = `SELECT * FROM USERS WHERE user_id='${user_id}'`;
   return userId_Select;
 };
 
 // 회원가입
-const insertUser = async (user_data) => {
+const insertUser = (user_data) => {
   const { socialId, type, user_name, profile_url } = user_data;
   const userInsert = `INSERT INTO USERS(user_id,social_id,social_type,user_name,profile_url)\ 
   VALUES('${createRandomId()}','${socialId}','${type}','${user_name}','${profile_url}');`;
   return userInsert;
 };
 
-const updateProfile = async (user_data) => {
+const updateProfile = (user_data) => {
   const { user_id, user_name, profile_url, profile_color } = user_data;
   const profileUpdate = `UPDATE USERS SET\
   user_name=${user_name} profile_url=${profile_url} profile_color=${profile_color}\
@@ -27,14 +27,14 @@ const updateProfile = async (user_data) => {
   return profileUpdate;
 };
 
-const updateConnect = async (logout_Data) => {
+const updateConnect = (logout_Data) => {
   const { connect_Date } = logout_Data;
   const logoutUpdate = `UPDATE USERS SET\
   is_connect=0 last_connected_at=${connect_Date} WHERE user_id=${user_id}
   `;
 };
 
-const deleteProfile = async (user_id) => {
+const deleteProfile = (user_id) => {
   const profile_Delete = `DELETE FROM USERS WHERE user_id='${user_id}';`;
   const refreshToken_Delete = `DELETE FROM USERS_REFRESH_TOKENS WHERE user_id='${user_id}'`;
   return profile_Delete + refreshToken_Delete;
