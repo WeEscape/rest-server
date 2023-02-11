76450f4bf7360c5f459d5a7361d6cd9104a77cd2
JoEunsoo <dcpg95@gmail.com>
2023-02-11 23:07:03 +0900

    REFACTOR : typescript 리팩토링

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,19 +1,19 @@
 import { TableQuery } from '../models/database';
 import { UsersModel } from '../models/users.model';
 
-const getUser = async (user_id) => {
+const getUser = async (user_id: string) => {
   const sql = UsersModel.selectUserInfo(user_id);
   const user = await TableQuery(sql);
-  return user[0];
+  return user;
 };
 
-const editUserProfile = async (update_date) => {
+const editUserProfile = async (update_date: string) => {
   const sql = UsersModel.updateProfile(update_date);
   const user = await TableQuery(sql);
   return user;
 };
 
-const deleteUser = async (user_id) => {
+const deleteUser = async (user_id: string) => {
   const sql = UsersModel.deleteProfile(user_id);
   const success = await TableQuery(sql);
   return success;
