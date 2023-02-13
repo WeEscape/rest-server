3bfcff79b7a58ddbda67ebd3b10673c38f1c0fb3
JoEunsoo <dcpg95@gmail.com>
2023-02-13 13:57:45 +0900

    CHORE : PoolOptions 타입추가

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
+import { PoolOptions } from 'mysql2';
 dotenv.config();
 
-export const dbconfig = {
+export const dbconfig: PoolOptions = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: 3306,
   connectionLimit: 10,
-  dateStrings: 'date',
+  dateStrings: ['DATE'],
   multipleStatements: true,
 };
