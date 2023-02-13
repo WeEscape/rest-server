fea0d427b5aac23654a98a83c0ce23389c992475
JoEunsoo <dcpg95@gmail.com>
2023-02-13 13:58:56 +0900

    CHORE : jwt payload 타입 추가

--- src/utils/decodeToken.util.ts
+++ src/utils/decodeToken.util.ts
@@ -1,11 +1,13 @@
+import jwt from 'jsonwebtoken';
 import { secretKey } from '../config/jwt.config';
 
-export const decodeAccessToken = async (token) => {
-  jwt
-  try {
-    const jwtDecode = jwt.verify(token, secretKey);
-    return jwtDecode;
-  } catch (err) {
-    return err;
+declare module 'jsonwebtoken' {
+  export interface UserIDJwtPayload extends jwt.JwtPayload {
+    user_id: string;
   }
+}
+
+export const decodeAccessToken = (token: string) => {
+  const { user_id } = <jwt.UserIDJwtPayload>jwt.verify(token, secretKey!);
+  return user_id;
 };
