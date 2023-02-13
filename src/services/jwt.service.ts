5c996102be7d0e927e6b65078f0f870a64be3a46
JoEunsoo <dcpg95@gmail.com>
2023-02-13 13:58:37 +0900

    CHORE : 파라미터 타입 추가

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,24 +1,20 @@
 import jwt from 'jsonwebtoken';
-import {
-  access_option,
-  refresh_option,
-  secretKey,
-} from '../config/jwt.config';
+import { access_option, refresh_option, secretKey } from '../config/jwt.config';
 import { TableQuery } from '../models/database';
 import { RefreshTokenModel } from '../models/refreshToken.model';
 import { getDate } from '../utils/getDate.util';
 
-export const createAccessToken = async (user_id) => {
-  const payload = { id: user_id };
-  const access_token = jwt.sign(payload, secretKey, access_option);
+export const createAccessToken = async (user_id: string) => {
+  const payload = { user_id };
+  const access_token = jwt.sign(payload, secretKey!, access_option);
   return access_token;
 };
 
-export const createRefreshToken = async (user_id) => {
+export const createRefreshToken = async (user_id: string) => {
   const selectSQL = RefreshTokenModel.selectUser_id(user_id);
-  const checkRefreshToken = await TableQuery(selectSQL);
+  const checkRefreshToken: any = await TableQuery(selectSQL);
 
-  const refresh_token = jwt.sign({}, secretKey, refresh_option);
+  const refresh_token = jwt.sign({}, secretKey!, refresh_option);
   const expiredDate = await getDate('expired');
   const user_ip = '123.456.789';
   const refreshtoken_data = { user_id, user_ip, refresh_token, expiredDate };
