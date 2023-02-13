db37f924aff5f735c73a9f3c34f22cf6f464527a
JoEunsoo <dcpg95@gmail.com>
2023-02-13 13:59:11 +0900

    CHORE : 파라미터 타입 추가

--- src/utils/checkHeader.utll.ts
+++ src/utils/checkHeader.utll.ts
@@ -1,8 +1,10 @@
+import { JwtPayload } from 'jsonwebtoken';
 import { decodeAccessToken } from './decodeToken.util';
 
-export const checkAccessToken = async (req) => {
+export const checkAccessToken = async (req: any) => {
   const request_header = req.headers['authorization'];
   const access_token = request_header.split(' ')[1];
-  const { id: user_id } = await decodeAccessToken(access_token);
+  const user_id = decodeAccessToken(access_token);
+
   return user_id;
 };
