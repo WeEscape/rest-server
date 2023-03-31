ab8703e75410fd14e5e9ed9498a86f98eb47722f
JoEunsoo <dcpg95@gmail.com>
2023-04-01 00:20:15 +0900

    REFACTOR : 루트 라우터 로직 function -> class

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -1,14 +1,21 @@
 import express, { Router, Request, Response } from 'express';
 import { logger } from '../config/logger.config';
-const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  logger.info('GET /');
-  return res.send('hello world!');
-});
-// router.get('/user');
-// router.post('/user');
-// router.put('/user');
-// router.delete('/user');
+class IndexRouter {
+  private router: Router;
 
-export default router;
+  constructor() {
+    this.router = express.Router();
+    this.getRouter();
+  }
+
+  public getRouter() {
+    this.router.get('/', (req: Request, res: Response) => {
+      logger.info('GET /');
+      return res.send('hello world!');
+    });
+    return this.router;
+  }
+}
+
+export default IndexRouter;
