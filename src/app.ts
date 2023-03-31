c70fc240b50aecb1ee978281431f42984cfeef9e
JoEunsoo <dcpg95@gmail.com>
2023-04-01 00:14:20 +0900

    REFACTOR : 서버 구성 class로 변경

--- src/app.ts
+++ src/app.ts
@@ -3,30 +3,52 @@ import cors from 'cors';
 import router from './routes/index.router';
 import userRouter from './routes/user.router';
 import authRouter from './routes/auth.router';
-import appConfig from './config/app.config';
+import { appConfig } from './config/app.config';
 import helmet from 'helmet';
 import { logger } from './config/logger.config';
 import groupRouter from './routes/group.router';
 import taskRouter from './routes/task.router';
+import { AppConfig } from './interfaces/config/app.interface';
+import IndexRouter from './routes/index.router';
 
-const app = express();
-const { port } = appConfig;
+export class App {
+  private app: express.Application;
+  private port: AppConfig;
+  private indexRouter: IndexRouter;
 
-app.use(cors());
-app.use(express.json());
-app.use(helmet());
+  constructor() {
+    this.app = express();
+    this.port = appConfig;
+    this.indexRouter = new IndexRouter();
+    this.middleware();
+    this.routers();
+  }
 
-app.use('/', router);
-app.use('/users/profile', userRouter);
-app.use('/auth', authRouter);
-app.use('/groups', groupRouter);
-app.use('/task', taskRouter);
+  middleware(): void {
+    this.app.use(express.json());
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.log(err);
-  res.status(400).send('err');
-});
+  /** app router 설정 */
+  routers(): void {
+    this.app.use('/', this.indexRouter.getRouter());
+    this.app.use('/users/profile', userRouter);
+    this.app.use('/auth', authRouter);
+    this.app.use('/groups', groupRouter);
+    this.app.use('/task', taskRouter);
+  }
 
-app.listen(port, () =>
-  logger.info(`Open Server Port ${process.env.PORT || port}`),
-);
+  async serverMain(): Promise<void> {
+    this.app.listen(this.port, () =>
+      logger.info(`Open Server Port ${process.env.PORT || this.port}`),
+    );
+  }
+}
+
+const serverStart = async (): Promise<void> => {
+  const app = new App();
+  await app.serverMain();
+};
+
+serverStart();
