0b889d2c1d98c8c8b1506ff61725104f9df47c81
JoEunsoo <dcpg95@gmail.com>
2023-04-07 01:04:02 +0900

    REFACTOR : 라우터 경로 및 변수명 변경

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import router from './routes/index.router';
+import IndexRouter from './routes/index.router';
 import userRouter from './routes/user.router';
 import authRouter from './routes/auth.router';
 import { appConfig } from './config/app.config';
@@ -9,17 +9,16 @@ import { logger } from './config/logger.config';
 import groupRouter from './routes/group.router';
 import taskRouter from './routes/task.router';
 import { AppConfig } from './interfaces/config/app.interface';
-import IndexRouter from './routes/index.router';
+
+import TaskRouter from './routes/task.router';
 
 export class App {
   private app: express.Application;
   private port: AppConfig;
-  private indexRouter: IndexRouter;
 
   constructor() {
     this.app = express();
     this.port = appConfig;
-    this.indexRouter = new IndexRouter();
     this.middleware();
     this.routers();
   }
@@ -32,11 +31,11 @@ export class App {
 
   /** app router 설정 */
   routers(): void {
-    this.app.use('/', this.indexRouter.getRouter());
+    this.app.use('/', IndexRouter);
     this.app.use('/users/profile', userRouter);
     this.app.use('/auth', authRouter);
     this.app.use('/groups', groupRouter);
-    this.app.use('/task', taskRouter);
+    this.app.use('/task', TaskRouter);
   }
 
   async serverMain(): Promise<void> {
