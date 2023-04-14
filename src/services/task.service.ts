b58f561f4c7c7f5b6027baacdfe1b1e7810d0724
JoEunsoo <dcpg95@gmail.com>
2023-04-15 03:19:21 +0900

    REFACTOR : taskservice 로직 class 로 변경

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -2,35 +2,28 @@ import { TaskInterface } from './../interfaces/task/task.interface';
 import { TableQuery } from '../models/database';
 import { TaskModel } from '../models/tasks.model';
 
-const createTask = async (taskData :string) => {
-  const sql = TaskModel.insertTask(taskData);
-  const task: any = await TableQuery(sql);
-  return task[1][0];
-};
-const getTask = async (taskData: TaskInterface) => {
-  const sql = TaskModel.selectTask(taskData);
-  const task: any = await TableQuery(sql);
-  return { data: task[0] };
-};
-const editTask = async (taskData: any) => {
-  const sql = TaskModel.updateTask(taskData);
-  const task: any = await TableQuery(sql);
-  return task[1][0];
-};
-const deleteTask = async (taskData: TaskInterface) => {
-  const sql = TaskModel.deleteTask(taskData);
-  const task: any = await TableQuery(sql);
-  return task;
-};
+export class TaskService {
+  async createTask(taskData: TaskInterface) {
+    const sql = TaskModel.insertTask(taskData);
+    const task: any = await TableQuery(sql);
+    return task[1][0];
+  }
 
-const createTaskUser = async () => {};
-const selectTaskUser = async () => {};
-const editTaskUser = async () => {};
-const deleteTaskUser = async () => {};
+  async getTask(taskData: TaskInterface) {
+    const sql = TaskModel.selectTask(taskData);
+    const task: any = await TableQuery(sql);
+    return { data: task[0] };
+  }
 
-export const TaskService = {
-  createTask,
-  getTask,
-  editTask,
-  deleteTask,
-};
+  async editTask(taskData: TaskInterface) {
+    const sql = TaskModel.updateTask(taskData);
+    const task: any = await TableQuery(sql);
+    return task[1][0];
+  }
+
+  async deleteTask(taskData: TaskInterface) {
+    const sql = TaskModel.deleteTask(taskData);
+    const task: any = await TableQuery(sql);
+    return task;
+  }
+}
