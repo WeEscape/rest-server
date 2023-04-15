6e846ac38ae826f2f09d2ab0fefe5c96989d7b09
JoEunsoo <dcpg95@gmail.com>
2023-04-15 23:16:03 +0900

    REFACTOR : authService class 구문으로 변경

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,69 +6,60 @@ import { getDate } from '../utils/getDate.util';
 import { createAccessToken, createRefreshToken } from './jwt.service';
 import { Oauth } from './axios.service';
 
-// 회원가입
-const signup = async (socialTokens: any) => {
-  const userInfo = await Oauth(socialTokens);
-  const sql = UsersModel.insertUser(userInfo);
-  const runSQL = await TableQuery(sql);
-  return runSQL;
-};
-
-// 로그인
-const login = async (loginUserData: any) => {
-  const userInfo: any = await Oauth(loginUserData);
-  const { socialId } = userInfo;
-
-  const sql = UsersModel.selectUserid(socialId);
-  const loginUser: any = await TableQuery(sql);
-
-  const { user_id } = loginUser[0][0];
-
-  if (!user_id) {
-    throw Error('uesr_id not defined');
+export class AuthService {
+  async signup(socialTokens: any) {
+    const userInfo = await Oauth(socialTokens);
+    const sql = UsersModel.insertUser(userInfo);
+    const runSQL = await TableQuery(sql);
+    return runSQL;
   }
 
-  const access_token = await createAccessToken(user_id);
-  const refresh_token = await createRefreshToken('user_id');
-  return { access_token, refresh_token };
-};
+  async login(loginUserData: any) {
+    const userInfo: any = await Oauth(loginUserData);
+    const { socialId } = userInfo;
 
-// 로그아웃
-const logout = async (logoutUser_id: any) => {
-  const { user_id } = logoutUser_id;
-  const connect_Date = getDate('');
+    const sql = UsersModel.selectUserid(socialId);
+    const loginUser: any = await TableQuery(sql);
 
-  if (!user_id) {
-    throw new Error('user_id not defined');
+    const { user_id } = loginUser[0][0];
+
+    if (!user_id) {
+      throw Error('uesr_id not defined');
+    }
+
+    const access_token = await createAccessToken(user_id);
+    const refresh_token = await createRefreshToken('user_id');
+
+    return { access_token, refresh_token };
   }
 
-  const logoutData = { user_id, connect_Date };
+  async logout(logoutUser_id: any) {
+    const { user_id } = logoutUser_id;
+    const connect_Date = getDate('');
 
-  const sql = UsersModel.updateConnect(logoutData);
-  const runSQL = await TableQuery(sql);
+    if (!user_id) {
+      throw new Error('user_id not defined');
+    }
 
-  return runSQL;
-};
+    const logoutData = { user_id, connect_Date };
+    const sql = UsersModel.updateConnect(logoutData);
+    const runSQL = await TableQuery(sql);
 
-// 토큰 재발급
-const reissueTokens = async (refreshToken: any) => {
-  const sql = RefreshTokenModel.selectRefresh_Token(refreshToken);
-  const runSQL: any = await TableQuery(sql);
-
-  if (!runSQL) {
-    throw { message: 'refresh_token Not defiend!' };
+    return runSQL;
   }
 
-  const { user_id } = runSQL[0];
-  const access_token = await createAccessToken(user_id);
-  const refresh_token = await createRefreshToken(user_id);
+  async reissueTokens(refreshToken: any) {
+    const sql = RefreshTokenModel.selectRefresh_Token(refreshToken);
+    const runSQL: any = await TableQuery(sql);
 
-  return { access_token, refresh_token };
-};
+    if (!runSQL) {
+      throw { message: 'refresh_token Not defiend!' };
+    }
 
-export const AuthService = {
-  signup,
-  login,
-  logout,
-  reissueTokens,
-};
+    const { user_id } = runSQL[0];
+    const access_token = await createAccessToken(user_id);
+    const refresh_token = await createRefreshToken(user_id);
+
+    return { access_token, refresh_token };
+  }
+}
