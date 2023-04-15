fbb26363b04028957006721999110cd3878b9eff
JoEunsoo <dcpg95@gmail.com>
2023-04-15 23:18:17 +0900

    REFACTOR : UserService 로직을 객체지향으로 변경

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,26 +1,22 @@
 import { TableQuery } from '../models/database';
 import { UsersModel } from '../models/users.model';
 
-const getUser = async (user_id: string) => {
-  const sql = UsersModel.selectUserInfo(user_id);
-  const user = await TableQuery(sql);
-  return user;
-};
+export class UserService {
+  async getUser(user_id: string) {
+    const sql = UsersModel.selectUserInfo(user_id);
+    const user = await TableQuery(sql);
+    return user;
+  }
 
-const editUserProfile = async (update_date: string) => {
-  const sql = UsersModel.updateProfile(update_date);
-  const user = await TableQuery(sql);
-  return user;
-};
+  async editUserProfile(update_date: string) {
+    const sql = UsersModel.updateProfile(update_date);
+    const user = await TableQuery(sql);
+    return user;
+  }
 
-const deleteUser = async (user_id: string) => {
-  const sql = UsersModel.deleteProfile(user_id);
-  const success = await TableQuery(sql);
-  return success;
-};
-
-export const UserService = {
-  getUser,
-  editUserProfile,
-  deleteUser,
-};
+  async deleteUser(user_id: string) {
+    const sql = UsersModel.deleteProfile(user_id);
+    const success = await TableQuery(sql);
+    return success;
+  }
+}
