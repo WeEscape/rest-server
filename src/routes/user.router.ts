21d25498d96aa813e370bf65986df0606e886a18
JoEunsoo <dcpg95@gmail.com>
2023-04-18 11:37:11 +0900

    REFACTOR : UserRouter 로직 분리 및 객체지향으로 변경

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,65 +1,85 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { userValidation } from '../middleware/validation/validation';
+import { Router } from 'express';
+import { UserController } from '../controllers/user.controller';
 import { UserService } from '../services/user.service';
-import { checkAccessToken } from '../utils/checkHeader.utll';
 
-const userRouter = express.Router();
+// const userRouter = express.Router();
 
-userRouter.get(
-  '/',
-  userValidation.readUser,
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user_id = await checkAccessToken(req) || '';
+// userRouter.get(
+//   '/',
+//   userValidation.readUser,
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const user_id = await checkAccessToken(req) || '';
 
-      if (!user_id) {
-        throw new Error('undefined');
-      }
+//       if (!user_id) {
+//         throw new Error('undefined');
+//       }
 
-      const resultUserInfo = await UserService.getUser(user_id);
+//       const resultUserInfo = await UserService.getUser(user_id);
 
-      return res.status(200).send({ data: resultUserInfo });
-    } catch (err) {
-      next(err);
-      return res.status(400).send({ message: 'access_token is not defined' });
-    }
-  },
-);
+//       return res.status(200).send({ data: resultUserInfo });
+//     } catch (err) {
+//       next(err);
+//       return res.status(400).send({ message: 'access_token is not defined' });
+//     }
+//   },
+// );
 
-userRouter.put(
-  '/',
-  userValidation.updateUser,
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const update_date = req.body();
-      const resultUserInfo = await UserService.editUserProfile(update_date);
-      return res.status(200).send({ data: resultUserInfo });
-    } catch (err) {
-      next(err);
-      return res.status(400).send({ message: err });
-    }
-  },
-);
+// userRouter.put(
+//   '/',
+//   userValidation.updateUser,
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const update_date = req.body();
+//       const resultUserInfo = await UserService.editUserProfile(update_date);
+//       return res.status(200).send({ data: resultUserInfo });
+//     } catch (err) {
+//       next(err);
+//       return res.status(400).send({ message: err });
+//     }
+//   },
+// );
 
-userRouter.delete(
-  '/',
-  userValidation.deleteUser,
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user_id = await checkAccessToken(req);
+// userRouter.delete(
+//   '/',
+//   userValidation.deleteUser,
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const user_id = await checkAccessToken(req);
 
-      if (!user_id) {
-        throw new Error('undefined');
-      }
+//       if (!user_id) {
+//         throw new Error('undefined');
+//       }
 
-      const resultDelete = await UserService.deleteUser(user_id);
+//       const resultDelete = await UserService.deleteUser(user_id);
 
-      return res.status(200).send({ message: 'success delete' });
-    } catch (err) {
-      next(err);
-      return res.status(400).send({ message: 'access_token is not defined' });
-    }
-  },
-);
+//       return res.status(200).send({ message: 'success delete' });
+//     } catch (err) {
+//       next(err);
+//       return res.status(400).send({ message: 'access_token is not defined' });
+//     }
+//   },
+// );
 
-export default userRouter;
+// export default userRouter;
+
+class UserRouter {
+  router: Router = Router();
+  userController: UserController;
+
+  constructor() {
+    const userService = new UserService();
+    this.userController = new UserController(userService);
+    this.initRoutes();
+  }
+
+  initRoutes() {
+    this.router.get('/', this.userController.getUser);
+    this.router.put('/', this.userController.updateUser);
+    this.router.delete('/', this.userController.deleteUser);
+  }
+}
+
+const userRouter = new UserRouter();
+
+export default userRouter.router;
